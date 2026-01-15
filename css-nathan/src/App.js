b838e78fdd62d131f8a98ce91bf9073c44d78e00
n4ttttt <nathanl34n@gmail.com>
2026-01-15 11:42:45 +0700

    test

---
+++ css-nathan/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='container'>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-1-v1.webp' alt='image' id='a'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-2-v1.webp' alt='image' id='b'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-3-v1.webp' alt='image' id='c'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-4-v1.webp' alt='image' id='d'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-5-v1.webp' alt='image' id='e'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-6-v2.webp' alt='image' id='f'></img>
+      <img src='https://d3o31au25zfcly.cloudfront.net/newfileadmin/usp/xp06/sec2/pc/grid-7-v1.webp' alt='image' id='g'></img>
+      <h1 className='text' id='aa'>Wireless MagPower</h1>
+      <h1 className='text' id='bb'>AI charging</h1>
+      <h2 className='text' id='cc'>Wireless Charge Max</h2>
+      <h2 className='text' id='dd'>Ultra Slim Crafted from Aluminum Alloy</h2>
+      <h1 className='text' id='ee'>600+ Times Charge Cycles</h1>
+      <h2 className='text' id='ff'>Dual Mode for Wireless & Cable</h2>
+      <h1 className='text' id='gg'>Safe & Secure</h1>
+      <h2 className='text' id='hh'>Certification</h2>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
